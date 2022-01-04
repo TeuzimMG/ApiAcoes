99e597a6e24e882517b8e134e8994aef57abb093
Matheus Magalhães <basemagalha@gmail.com>
2022-01-03 22:02:20 -0300

    Edit

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from prettytable import PrettyTable
 import json
 import matplotlib.pyplot as plt
 import numpy as np
+from requests.api import options
 
 
 url = "https://www.alphavantage.co"
@@ -19,13 +20,14 @@ banner = """
 ////////////////////////////////
 """
 
-forex = """
+options = """
    | 1 | FX_DAILY
    | 2 | FX_WEEKLY
    | 3 | FX_MONTHLY
    | 4 | FX_INTRADAY 
    
-
+   | 1 | DIGITAL_CURRENCY_DAILY
+   
 
 """
 
@@ -45,49 +47,64 @@ class Api:
       response_dict = json.loads(response.text)
       return [response,response_dict]
    
+   def consult_crypto(self,function,symbol,market,**kwargs):
+      urlconsult = url+f'/query?function={function}&symbol={symbol}&market={market}&apikey={self.apikey}'
+      if kwargs.items():
+         for key, value in kwargs.items():
+            urlconsult += f'&{key}={value}'
+      response = requests.get(urlconsult)
+      response_dict = json.loads(response.text)
+      return [response,response_dict]
+   
 key = str(input('Api-Key:'))
 api = Api(url,key)
 while True:
    os.system('cls')
    print(banner)
-   print(forex)
+   print(options)
    function = str(input('Qual função deseja:'))
-   if function.strip() == ('FX_DAILY' or 'FX_WEEKLY' or 'FX_MONTHLY'):
+   if function.strip() == 'FX_DAILY' or 'FX_WEEKLY' or 'FX_MONTHLY':
       from_money = str(input('De moeda:'))
       to_money = str(input('Para moeda:'))
       dias = int(input('Quantos dias quer ver:').strip())
       response = api.consult_forex(function,from_money,to_money)
-      dados = response[1]['Time Series FX (Daily)']
-      total = 0
-      closing = []
-      high = []
-      low = []
-      up = []
-      date = []
-      opening = []
-      for c in dados:
-         total += 1
-         date.append(c)
-         high.append(float(dados[c]['2. high']))
-         low.append(float(dados[c]['3. low']))
-         up.append(float(dados[c]['1. open'])-float(dados[c]['4. close']))
-         opening.append(float(dados[c]['1. open']))
-         closing.append(float(dados[c]['4. close']))
-         if total == dias:
-            break
-      print('DADOS > \n')
-      x = PrettyTable()
-      x.add_column('Datas', date)
-      x.add_column('Abertura', opening)
-      x.add_column('Alto', high)
-      x.add_column('Baixo', low)
-      x.add_column('Fechamento',closing)
-      x.add_column('Ganho',up)
-      x.align['Abertura'] = 'r'
-      x.align['Alto'] = 'r'
-      x.align['Baixo'] = 'r'
-      x.align['Fechamento'] = 'r'
-      x.align['Ganho'] = 'r'
-      print(x)
-      
-      teste = input('Tecle enter para continuar com as consultas')
+   if function.strip() == 'DIGITAL_CURRENCY_DAILY':
+      symbol = str(input('De moeda:'))
+      market = str(input('Para moeda:'))
+      dias = int(input('Quantos dias quer ver:').strip())
+      response = api.consult_crypto(function,from_money,to_money)
+   dados = response[1]
+   dados = list(dados.values())[1]
+   total = 0
+   closing = []
+   high = []
+   low = []
+   up = []
+   date = []
+   opening = []
+   for c in dados:
+      total += 1
+      date.append(c)
+      high.append(float(dados[c]['2. high']))
+      low.append(float(dados[c]['3. low']))
+      up.append(float(dados[c]['1. open'])-float(dados[c]['4. close']))
+      opening.append(float(dados[c]['1. open']))
+      closing.append(float(dados[c]['4. close']))
+      if total == dias:
+         break
+   print('DADOS > \n')
+   x = PrettyTable()
+   x.add_column('Datas', date)
+   x.add_column('Abertura', opening)
+   x.add_column('Alto', high)
+   x.add_column('Baixo', low)
+   x.add_column('Fechamento',closing)
+   x.add_column('Ganho',up)
+   x.align['Abertura'] = 'r'
+   x.align['Alto'] = 'r'
+   x.align['Baixo'] = 'r'
+   x.align['Fechamento'] = 'r'
+   x.align['Ganho'] = 'r'
+   print(x)
+   
+   teste = input('Tecle enter para continuar com as consultas')
